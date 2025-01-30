5c1d880fd6dc7803aff175044ec04f7ff20fbaf3
Marta de la Calle <martadelacalle2003@gmail.com>
2025-01-31 00:45:20 +0100

    update: project page

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,71 @@
+import './css/projects.css'
+import { useLanguage } from './utils/LenguageProvider';
+import { translations } from './utils/translations';
+
 export default function Projects(){
-    
+    const { language } = useLanguage();
+
+    return(
+        <div className="projects-container">
+            <div className="project-left">
+                <div className="project-image">
+                    <div className="rectangle-project"></div>  
+                    <img className="photo-project" alt="YuGi-Oh!" src={""} />
+                </div>  
+                <div className="project-text">
+                    <h3 className="project-title">YuGi-Oh!</h3>
+                    <p className="project-description">{translations[language].yugioh}</p>
+                    <p className="project-description">{translations[language].yh1}</p>
+                    <p className="project-description">{translations[language].yh2}</p>
+                </div>  
+            </div>  
+            <div className="project-right">
+                <div className="project-image">
+                    <img className="photo-project" alt="INFOCA" src={""}/>
+                    <div className="rectangle-project"></div>  
+                </div> 
+                <div className="project-text">
+                    <h3 className="project-title">Plan INFOCA</h3> 
+                    <p className="project-description">{translations[language].infoca}</p> 
+                    <p className="project-description">{translations[language].if1}</p>
+                </div> 
+            </div> 
+            <div className="project-left">
+                <div className="project-image">
+                    <div className="rectangle-project"></div>  
+                    <img className="photo-project" alt="VideoMiner" src={""}/>
+                </div> 
+                <div className="project-text">
+                    <h3 className="project-title">VideoMiner</h3> 
+                    <p className="project-description">{translations[language].videominer}</p> 
+                    <p className="project-description">{translations[language].vm1}</p>
+                    <p className="project-description">{translations[language].vm2}</p>
+                </div> 
+            </div> 
+            <div className="project-right">
+                <div className="project-image">
+                    <img className="photo-project" alt="Las Flores No Se Comen" src={""}/>
+                    <div className="rectangle-project"></div>
+                </div>
+                <div className="project-text">
+                    <h3 className="project-title">Las Flores No Se Comen</h3>  
+                    <p className="project-description">{translations[language].lasfloresnosecomen}</p>
+                    <p className="project-description">{translations[language].lfnsc1}</p>
+                    <p className="project-description">{translations[language].lfnsc2}</p>
+                </div> 
+            </div> 
+            <div className="project-left">
+                <div className="project-image">
+                    <div className="rectangle-project"></div>  
+                    <img className="photo-project" alt="PLANTY" src={""}/>
+                </div> 
+                <div className="project-text">
+                    <h3 className="project-title">PLANTY</h3>  
+                    <p className="project-description">{translations[language].planty}</p>
+                    <p className="project-description">{translations[language].pt1}</p>
+                    <p className="project-description">{translations[language].pt2}</p>
+                </div> 
+            </div> 
+        </div>
+    );  
 }
